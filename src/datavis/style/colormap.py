9b52d50ea47d7829f308b507d25b27e6302104a8
FeiYu <1051685967@qq.com>
2022-09-28 21:06:40 +0800

    add color

--- src/datavis/style/colormap.py
+++ src/datavis/style/colormap.py
@@ -3,6 +3,7 @@ from .base import BaseEnum
 
 
 class ColorMaps(BaseEnum):
+    jet = Info("jet", matplotlib="jet")
     jet_r = Info("jet_r", matplotlib="jet_r")
     gray = Info("gray", matplotlib="gray")
     gray_r = Info("gray_r", matplotlib="gray_r")
